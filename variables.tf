b0fa3f62db58efaf8e41aba4d3a59279adc4a988
nichollascorrealab <93170056+nichollascorrealab@users.noreply.github.com>
2021-11-01 18:59:55 -0300

    Commit variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "aws_region" {
-  description = "The AWS region to create things in."
+  description = "Região para Criação no AWS"
   default     = "us-east-2"
 }
 
 variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  description = "Chave SSH"
+  default     =  "lab_cert"
 }
 
 variable "instance_type" {
-  description = "instance type for ec2"
+  description = "Tipo de Instância"
   default     =  "t2.micro"
 }
 
 variable "security_group" {
-  description = "Name of security group"
-  default     = "my-jenkins-security-group"
+  description = "Nome do Grupo de Segurança"
+  default     = "sg-0027954aaddb8219d (launch-wizard-5)"
 }
 
 variable "tag_name" {
-  description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+  description = "Tag da Instância"
+  default     = "NICHOLLAS_LAB_TERRAFORM"
 }
 
 variable "ami_id" {
-  description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+  description = "Microsoft Windows Server 2019 Base"
+  default     = "ami-08c213ebdf7b857c2"
 }
